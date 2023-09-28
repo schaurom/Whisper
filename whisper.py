c31a9aa9adc17046624e2f1c01e6d732f47e748c
schaurom <118014904+schaurom@users.noreply.github.com>
2023-09-28 22:39:01 +0200

    .

---
+++ whisper.py
@@ -0,0 +1,11 @@
+import whisper
+import numpy as np
+
+model = whisper.load_model("medium")
+
+options = {"language": "de"}
+result = model.transcribe("./audio/audio.mp3", **options)
+#result = model.transcribe(r"C:\Users\schau\PycharmProjects\Whisper\whisper\audio.mp3", **options)
+print(result["text"])
+
+np.savetxt("./audio/audio_transcript.csv", result["text"])
