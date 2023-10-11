0aa61567212420834e523e3064d746ef4f0bccc4
schaurom <118014904+schaurom@users.noreply.github.com>
2023-10-11 22:31:09 +0200

    build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Aktualisiere pip und installiere Abhängigkeiten
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Führe Datenbankmigrationen durch (falls zutreffend)
+# python manage.py db upgrade
+
+# Führe Tests aus (falls zutreffend)
+# pytest
+
+# Sammle statische Dateien (falls deine App statische Dateien hat)
+# python manage.py collectstatic
+
+# Starte die Flask-Anwendung (verwende den richtigen Befehl für deine Anwendung)
+# python meincode.py oder gunicorn meincode:app
+python app.py
+
+# Du kannst auch hier spezifische Umgebungsvariablen oder Konfigurationen setzen
+# export FLASK_ENV=production
+# export FLASK_APP=meincode.py
+
+# Führe die Anwendung aus
+# python app.py
